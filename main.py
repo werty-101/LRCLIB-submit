63c25fbabc39259fc43ba49a621becbc0b8d6383
werty-101 <jose.garcia331@yahoo.com>
2026-05-18 14:06:19 -0400

    renamed BLACKLIST file name created test_change_lyrics() created convert_to_seconds added scroll effect thing in test_main()

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ import requests
 import string
 import os
 from time import sleep
-from dotenv import load_dotenv
 
 
-load_dotenv()
 SCREEN_SIZE = pydirectinput.size()
 MIDDLE_X, MIDDLE_Y = round(SCREEN_SIZE[0] * 0.5), round(SCREEN_SIZE[1] * 0.5)
-BLACKLIST = "blacklist.txt"  # used for song titles that return wrong lyrics so they can be added manually
+BLACKLIST = "to_be_added.txt"  # used for song titles that return wrong lyrics so they can be added manually
 SONGS_FOLDER = "lyrics_folder"  # todo: check if folder exists and check if its a folder item
 
 
@@ -27,7 +25,7 @@ def custom_triple_click(location_x: int, location_y: int):
 
 
 # activates the macro and moves mouse slightly to wake up the mouse
-def change_lyrics(time_start: int, lyric: str, time_end: int):
+def change_lyrics(time_start: float, lyric: str, time_end: float):
     # int(f"{(screen_size[0] * 0.5) :.0f}") looks ugly
     custom_triple_click(MIDDLE_X, MIDDLE_Y)
     subprocess.run('clip', text=True, input=lyric)
@@ -43,10 +41,18 @@ def change_lyrics(time_start: int, lyric: str, time_end: int):
     pydirectinput.click()
     sleep((time_end - time_start) - 0.01)
 
+def test_change_lyrics(time_start: float, lyric: str, time_end: float):
+    # so it needs two lyric items, lyric start [00:00.00] <Music>, [00:44.57] Living for you
+    # so it can do something like:
+    # print(lyric[0])
+    # sleep 44.57 - 0:00
+    print(lyric)
+    sleep(time_end - time_start)
+    pass
 
 # find lyrics using song title and author on local database, if fail search https://lrclib.net
 def find_lyrics(song_title: str, author: str = ''):
-    # todo: check if song is in blacklist.txt if yes go search on api
+    # todo: check if song is in to_be_added.txt if yes go search on api
 
     print()
     highest_match = 0
@@ -101,23 +107,30 @@ def find_lyrics(song_title: str, author: str = ''):
         # if search fails, search local storage
         # what to do abt false positives
         # add them to the blacklist, so they can later be manually added to local db
-        if r.status_code == 404:
-            print("song not found on lrclib")
-            return f"{song_title} not found in local db nor api"
-        elif r.status_code == 200:
+        if r.json() and r.status_code == 200:
             # fetched lyrics! download to db instead of printing
             # todo: add clean version of lyrics to database
-            #print(r.json()[0]["syncedLyrics"])
-            #print(os.path.join(SONGS_FOLDER, f"{author} - {song_title}.lrc"))
+            # if item exists / another try except (dont really want another tho)
+            print(r.json())
+            print(f"\nlyrics found! from: {r.json()[0]["artistName"]} title: {r.json()[0]["trackName"]}\n")
+            print("Lyrics:")
+            print(r.json()[0]["syncedLyrics"])
+            # print(os.path.join(SONGS_FOLDER, f"{author} - {song_title}.lrc"))
             if input("save lyrics? (y/n): ").lower() == 'y':
-                #f = open(os.path.join(SONGS_FOLDER, f"{author} - {song_title}.lrc"), "x")
+                # f = open(os.path.join(SONGS_FOLDER, f"{author} - {song_title}.lrc"), "x")
                 f = open(f"{SONGS_FOLDER}//{author} - {song_title}.lrc", 'x')
                 f.write(r.json()[0]["syncedLyrics"])
                 f.close()
                 print(f"saved lyrics to {SONGS_FOLDER} folder")
+        elif not r.json() and r.status_code == 200:
+            print("could not find lyrics using lrclib api")
+            return [f'could not find lyrics for {song_title}']
         else:
             print(f"an unknown error occurred: r.status_code = {r.status_code}")
 
+def convert_to_seconds(time: str):
+    seconds = time.translate(str.maketrans('','', "[]")).split(":")
+    return round(float((int(seconds[0]) * 60) + float(seconds[1])), 2)
 
 def main():
     attempts = 0
@@ -159,14 +172,29 @@ def test_main():
 
     print("in app")
     # after title and author fetched, def fetch_vid_title:
-    song_title = 'With you (1994)'
-    author = 'Helena'
-    synced_lyrics = find_lyrics(song_title, author)
-    for i in range(len(synced_lyrics)):
-        print(synced_lyrics[i])
+    test_song = 'With you (1994)'
+    test_author = 'Helena'
+    synced_lyrics = find_lyrics(test_song, test_author)
+    if synced_lyrics is not None:
+        # separate the timestamp from the lyric
+        pass
+        # convert timestamp to seconds
+        pass
+        # after separation do
+        for i in range(len(synced_lyrics)):
+            if i + 1 < len(synced_lyrics):
+                current_lyric = synced_lyrics[i].split(' ', 1)
+                timestamp = convert_to_seconds(current_lyric[0])
+                lyric_str = current_lyric[1]
+                next_timestamp = convert_to_seconds(synced_lyrics[i+1].split(' ', 1)[0])
+                test_change_lyrics(timestamp, lyric_str, next_timestamp)
+
+    else:
+        print("None returned")
 
 
 # to test use: Fever the Night by Matt Land, Virtual Love by Ken Martin, and With you (1994) by Helena
+# more tests: In My Dreams by Denise, Can't Stay A Dreamy Girl by Nikita Jr
 # also try to use mixes into the player
 if __name__ == '__main__':
     test_main()
