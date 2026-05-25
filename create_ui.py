d0b70079473b34e4db20269341537c5a9fa127ba
werty-101 <ID+username@users.noreply.github.com>
2026-05-24 22:55:58 -0400

    Create constants and imports delete random kid Create funct browse_files() Move assign_vars() Create File Search

--- create_ui.py
+++ create_ui.py
@@ -1,6 +1,10 @@
 # imports
 import tkinter as tk
 import submit_lyrics
+from tkinter import filedialog
+from pathlib import Path
+
+LYRICS_FOLDER = "lyrics_folder"
 
 # theme colors
 dark_mode_colors = {
@@ -11,7 +15,6 @@ dark_mode_colors = {
 
 }
 
-status = 404
 
 # Application class used in main()
 class Application(tk.Tk):
@@ -40,12 +43,40 @@ class Page1(Base):
     def __init__(self, parent):
         super().__init__(parent)
 
+        self.file_dir = ""
+
         self.pack(side="top", fill="x", expand=True)
         self.propagate(False)
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
 
-        print(self.grid_rowconfigure(0))
+        # look up .lrc and .txt files and insert them into the file entry field
+        def browse_files(entry_field):
+            file_name = filedialog.askopenfilename(initialdir=LYRICS_FOLDER,
+                                                   title="Browse File",
+                                                   filetypes=(
+                                                       ("Lyric Files", "*.lrc"),
+                                                       ("Text Files", "*.txt"),
+                                                       ("All Files", "*.*")
+                                                    )
+                                                   )
+            entry_field.insert(0, Path(file_name).name)
+            self.file_dir = file_name
+
+        # assign names to submit_lyrics using raw strings
+        def assign_vars(title, artist, album, duration, path):
+            # assign names to submit_lyrics
+            # USE RAW STRINGS
+            # todo: check if fields are filled
+            submit_lyrics.track_name = title.get()
+            submit_lyrics.artist_name = artist.get()
+            submit_lyrics.album_name = album.get()
+            submit_lyrics.duration = duration.get()
+            submit_lyrics.file_path = path
+            submit_lyrics.print_name()
+            submit_lyrics.synced_to_plain_lyrics()
+            # submit lyrics to function to like break down the lyrics
+            # send it to main
 
         # title label
         tk.Label(master=self, text="Submit Lyrics to LRCLIB",
@@ -77,7 +108,7 @@ class Page1(Base):
                     label_text = "none"
                     entry_key = -1
                 parent_frame = tk.Frame(master=self, background=dark_mode_colors["primary"], width=50, height=50)
-                parent_frame.grid(row=x, column=y, sticky="nw", padx=20, pady=(70, 60))
+                parent_frame.grid(row=x, column=y, sticky="nw", padx=20, pady=(50, 50))
                 tk.Label(master=parent_frame, text=label_text,
                          background=dark_mode_colors["primary"],
                          foreground=dark_mode_colors["highlight"]
@@ -86,26 +117,33 @@ class Page1(Base):
                 entry_value.pack(side="bottom", fill="both", expand=True)
                 entry_field_dict[entry_key] = entry_value
 
+        # file search frame
+        file_frame = tk.Frame(master=self, background=dark_mode_colors["primary"])
+        tk.Label(master=file_frame, text="Lyric File Path",
+                 background=dark_mode_colors["primary"],
+                 foreground=dark_mode_colors["highlight"]
+                 ).pack(anchor="nw", fill="y", expand=True)
+        file_entry = tk.Entry(master=file_frame, width=50, )
+        file_entry.pack(side="left", fill="x", expand=True)
+        file_browse_button = tk.Button(master=file_frame, text="Browse", width=10)
+        file_browse_button.config(command=lambda: browse_files(file_entry))  # save returned str as var
+        # something like (command=lambda: funct_return = browse_files(file_entry)
+        file_browse_button.pack(side="right", padx=(10, 0))
 
+        file_frame.grid(row=2, column=0, columnspan=2, sticky="n")
 
         # submit button
         b = tk.Button(master=self, text="Submit", width=10, height=2)
         b.config(command=lambda: assign_vars(entry_field_dict[0],
                                              entry_field_dict[1],
                                              entry_field_dict[2],
-                                             entry_field_dict[3]))
-        b.grid(row=3, column=0, columnspan=2, pady=(0, 70), sticky="n")
+                                             entry_field_dict[3],
+                                             self.file_dir))
+        b.grid(row=3, column=0, columnspan=2, pady=(20, 50), sticky="n")
 
-        def assign_vars(title, artist, album, duration):#, path):
-            # assign names to submit_lyrics
-            # USE RAW STRINGS
-            # todo: check if fields are filled
-            submit_lyrics.track_name = title.get()
-            submit_lyrics.artist_name = artist.get()
-            submit_lyrics.album_name = album.get()
-            submit_lyrics.duration = duration.get()
-            #submit_lyrics.file_path = path.get()
-            submit_lyrics.print_name()
+
+
+
 
 
 # MainView class, used as a container for frames, in case other pages are added onto app
