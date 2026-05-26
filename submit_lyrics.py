e3acc1606a18ab258fbe2495b515651d58f945b0
werty-101 <ID+username@users.noreply.github.com>
2026-05-26 16:28:32 -0400

    Add new imports and variables Create to_seconds() Finish synced_to_plain_lyrics() Finish main()

--- submit_lyrics.py
+++ submit_lyrics.py
@@ -1,25 +1,17 @@
 # imports
 import hashlib
+import json
 import requests
 
 # variables
 track_name = ""
 artist_name = ""
 album_name = ""
-duration = 0
+duration = int(0)
 file_path = ""
-# todo: get synced lyrics and plain lyrics from file_path
 # todo: verify if file path exists
-plain_lyrics = ""
 synced_lyrics = ""
-
-data = {"trackName": track_name,
-        "artistName": artist_name,
-        "albumName": album_name,
-        "duration": duration,
-        "plainLyrics": plain_lyrics,
-        "syncedLyrics": synced_lyrics
-        }
+plain_lyrics = ""
 
 
 # verifies nonce once calculated by solve_challenge
@@ -57,29 +49,54 @@ def solve_challenge(prefix: str, target_hex: str) -> str:
     return str(nonce)
 
 
-def print_name():
-    print(rf"{track_name}, {artist_name}, {album_name}, {duration}, {file_path}")
+# converts (hh:mm:ss) into seconds
+def to_seconds(dur):
+    if dur.__class__ == str:
+        if ":" in dur:
+            dur_split = dur.split(":", 2)
+            for i in range(len(dur_split)):
+                if i + 1 < len(dur_split):
+                    dur_split[i+1] = int(dur_split[i+1])
+                    dur_split[i+1] += int(dur_split[i]) * 60
+            return int(dur_split[len(dur_split) - 1])
+        # TODO: CHECK FOR CASES WHERE THERE IS A STRING THAT CONTAINS ":" PROB USE TRY
+        # TODO: fix issue where a song is less than a min long, ss comes up as str
+        else:
+            return "INVALID STRING"
+    elif dur.__class__ is float:
+        return int(round(dur))
+    elif dur.__class__ is int:
+        return dur
+    else:
+        print(dur, dur.__class__)
+        return "INVALID FORMAT"
 
 
 def synced_to_plain_lyrics():
-    # max split = 1
+    global plain_lyrics
+    global synced_lyrics
     plain_list = []
     with open(file_path, "r") as file:
+        synced_lyrics = file.read()
+    with open(file_path, "r") as file:
         lines_list = file.readlines()
-        for i in range(len(lines_list)):
-            try:
-                plain_list.append(lines_list[i].split(" ", maxsplit=1)[1])
-            except IndexError:
-                plain_list.append("\n")
-            print(plain_list[i])  # plain lyrics
-            # todo: convert stuff from plain_list into big string
+    for i in range(len(lines_list)):
+        try:
+            plain_list.append(lines_list[i].split(" ", maxsplit=1)[1])
+        except IndexError:
+            plain_list.append("\n")
 
-            #print(lines_list[i].split(" ", maxsplit=1))
+    plain_lyrics = "".join(plain_list)  # plain lyrics
+    return plain_lyrics
 
 # todo: check if song is already in LRCLIB by performing /api/get-cached with vars from above
 
 
 def main():
+
+    # TODO: check if fields are filled
+    # if val = "" then print('you must fill the required fields')
+
     url = "https://lrclib.net/api/request-challenge"
     header = {'User-Agent': 'rmm youtube bot v?.?.? (https://github.com/werty-101/rmm-youtube-bot)'}
 
@@ -91,9 +108,19 @@ def main():
 
     url = "https://lrclib.net/api/publish"
     header = {"X-Publish-Token": f"{prefix}:{nonce}",
+              "Content-Type": "application/json",
               "User-Agent": "rmm youtube bot v?.?.? (https://github.com/werty-101/rmm-youtube-bot)"}
+    data = {"trackName": track_name,
+            "artistName": artist_name,
+            "albumName": album_name,
+            "duration": to_seconds(duration),
+            "plainLyrics": synced_to_plain_lyrics(),
+            "syncedLyrics": synced_lyrics
+            }
 
-    r = requests.post(url, headers=header, data=data)
+    r = requests.post(url, headers=header, data=json.dumps(data))
+
+    print(r.status_code)
 
     if r.status_code == 201:
         print("OMGGG NO WAy")
